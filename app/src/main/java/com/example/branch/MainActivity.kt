7155f702579ccd0fbd77391f5a8d63eaeff663ce
Islamova12345 <gislamova@std.beu.edu.az>
2024-08-12 23:28:53 +0400

    branch1 bolme funksiyasi

--- app/src/main/java/com/example/branch/MainActivity.kt
+++ app/src/main/java/com/example/branch/MainActivity.kt
@@ -38,5 +38,19 @@ class MainActivity : AppCompatActivity() {
                 binding.ResultTextView.text = result.toString()
             }
         }
+        binding.BolmeButton.setOnClickListener {
+            val num1 = binding.editText1.text.toString().toDoubleOrNull()
+            val num2 = binding.editText2.text.toString().toDoubleOrNull()
+            if (num1 != null && num2 != null) {
+                if (num2 != 0.0) {
+                    val result = num1 / num2
+                    binding.ResultTextView.text = result.toString()
+                } else {
+                    binding.ResultTextView.text = "Cannot divide by zero"
+                }
+            }
         }
     }
+
+}
+
